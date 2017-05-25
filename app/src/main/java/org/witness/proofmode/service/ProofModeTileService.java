5bac7a7e36999a59604aff24c086a3bdbbb182cc
n8fr8 <nathan@freitas.net>
2017-05-25 16:15:46 -0400

    fix quick settings tile for Android 7

--- app/src/main/java/org/witness/proofmode/service/ProofModeTileService.java
+++ app/src/main/java/org/witness/proofmode/service/ProofModeTileService.java
@@ -3,28 +3,28 @@ package org.witness.proofmode.service;
 import android.annotation.TargetApi;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.Icon;
+import android.os.Build;
 import android.preference.PreferenceManager;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 
 import org.witness.proofmode.MainActivity;
+import org.witness.proofmode.R;
 
 /**
  * Created by n8fr8 on 2/23/17.
  */
-@TargetApi(24)
+@TargetApi(Build.VERSION_CODES.N)
 public class ProofModeTileService extends TileService {
 
     private SharedPreferences mPrefs;
-    private boolean isActive = false;
-    private Tile mTile;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
         mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-
     }
 
     @Override
@@ -35,39 +35,50 @@ public class ProofModeTileService extends TileService {
     @Override
     public void onTileAdded() {
         super.onTileAdded();
+
+        changeTileState(mPrefs.getBoolean("doProof",true)?Tile.STATE_ACTIVE:Tile.STATE_INACTIVE);
+        
     }
 
     @Override
-    public void onTileRemoved() {
-        super.onTileRemoved();
+    public void onClick() {
+
+        if (!isLocked()) {
+            updateTile();
+        } else {
+            unlockAndRun(new Runnable() {
+                @Override
+                public void run() {
+                    updateTile();
+                }
+            });
+        }
     }
 
     @Override
     public void onStartListening() {
         super.onStartListening();
-
-        updateTileState ();
+        changeTileState(getQsTile().getState());
     }
 
-    private void updateTileState ()
-    {
-        isActive = mPrefs.getBoolean("doProof",false);
 
-        mTile = getQsTile();
-        mTile.setState(isActive? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
-        mTile.updateTile();
+    private void updateTile() {
+        if (Tile.STATE_ACTIVE == getQsTile().getState()) {
+            changeTileState(Tile.STATE_INACTIVE);
+            mPrefs.edit().putBoolean("doProof",false).commit();
 
+        } else if (Tile.STATE_INACTIVE == getQsTile().getState()) {
+            changeTileState(Tile.STATE_ACTIVE);
+            mPrefs.edit().putBoolean("doProof",true).commit();
+        }
     }
 
-    @Override
-    public void onClick() {
-        super.onClick();
-
-        isActive = !isActive;
-        mPrefs.edit().putBoolean("doProof",isActive).commit();
-        updateTileState ();
-
+    private void changeTileState(int newState) {
+        getQsTile().setIcon(Icon.createWithResource(this, newState == Tile.STATE_INACTIVE ? R.drawable.proofmodegrey : R.drawable.proofmodewhite));
+        getQsTile().setState(newState);
+        getQsTile().updateTile();
     }
+
 
 
 }
