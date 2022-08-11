e556bfc3b5bd01573b157c4e09777e509fa7a4d1
n8fr8 <nathan@freitas.net>
2022-08-11 14:49:21 -0400

    update method signature

--- app/src/main/java/org/witness/proofmode/viewmodels/MainViewModelFactory.kt
+++ app/src/main/java/org/witness/proofmode/viewmodels/MainViewModelFactory.kt
@@ -4,11 +4,9 @@ import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-
-
 class MainViewModelFactory(private val app:Application) : ViewModelProvider.Factory {
 
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
             return MainViewModel(app) as T
         }
