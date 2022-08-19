268cd0d4717457b0c9abb4738a3db833b8a6ece2
n8fr8 <nathan@freitas.net>
2022-08-19 17:30:33 -0400

    update verify scripts

--- docs/verify.sh
+++ docs/verify.sh
@@ -10,4 +10,6 @@ gpg --no-default-keyring --keyring ./pubkey.asc.gpg --homedir ./ --verify $proof
 gpg --no-default-keyring --keyring ./pubkey.asc.gpg --homedir ./ --verify $proofhash.proof.csv.asc
 gpg --no-default-keyring --keyring ./pubkey.asc.gpg --homedir ./ --verify $proofhash.proof.json.asc 
 
+ots -v v $proofhash.ots -f $prooffile
 
+jq -R 'split(".") | select(length > 0) | .[0],.[1] | @base64d | fromjson' <<< cat $proofhash.gst
