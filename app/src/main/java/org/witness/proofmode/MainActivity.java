214f95556749ee67e5144f2270d94dd1146705b2
n8fr8 <nathan@freitas.net>
2022-11-17 13:07:37 -0500

    dont' ask for phone identity by default, the permission needs to be explained

--- app/src/main/java/org/witness/proofmode/MainActivity.java
+++ app/src/main/java/org/witness/proofmode/MainActivity.java
@@ -67,10 +67,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
      * The permissions needed for "base" ProofMode to work, without extra options.
      */
     private final static String[] requiredPermissions = new String[] {
-            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
     };
     private final static String[] optionalPermissions = new String[] {
-            Manifest.permission.READ_PHONE_STATE,
+    //        Manifest.permission.READ_PHONE_STATE,
             Manifest.permission.ACCESS_NETWORK_STATE,
             Manifest.permission.CAMERA
     };
