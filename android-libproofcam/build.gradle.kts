ed5ccce78178b920049de41a1681ee10f69f4e2f
n8fr8 <nathan@freitas.net>
2023-03-23 09:10:54 -0400

    update dependencies

--- android-libproofcam/build.gradle.kts
+++ android-libproofcam/build.gradle.kts
@@ -34,18 +34,18 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 
-    implementation("com.google.android.material:material:1.8.0-beta01")
+    implementation("com.google.android.material:material:1.9.0-alpha01")
 
-    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("androidx.appcompat:appcompat:1.6.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
 
-    implementation("androidx.camera:camera-core:1.1.0-beta03")
-    implementation("androidx.camera:camera-camera2:1.1.0-beta03")
-    implementation("androidx.camera:camera-lifecycle:1.1.0-beta03")
-    implementation("androidx.camera:camera-extensions:1.1.0-beta03")
-    implementation("androidx.camera:camera-view:1.1.0-beta03")
+    implementation("androidx.camera:camera-core:1.3.0-alpha03")
+    implementation("androidx.camera:camera-camera2:1.3.0-alpha03")
+    implementation("androidx.camera:camera-lifecycle:1.3.0-alpha03")
+    implementation("androidx.camera:camera-extensions:1.3.0-alpha03")
+    implementation("androidx.camera:camera-view:1.3.0-alpha03")
 
     implementation("androidx.fragment:fragment:1.5.5")
     implementation("androidx.fragment:fragment-ktx:1.5.5")
