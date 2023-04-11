2fdccf3c324fca55b1c8b6a3effb30fedbfc0fde
n8fr8 <nathan@freitas.net>
2023-04-10 22:11:09 -0400

    update dependencies

--- android-libproofcam/build.gradle.kts
+++ android-libproofcam/build.gradle.kts
@@ -34,24 +34,24 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 
-    implementation("com.google.android.material:material:1.9.0-alpha01")
+    implementation("com.google.android.material:material:1.9.0-beta01")
 
-    implementation("androidx.appcompat:appcompat:1.6.0")
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.core:core-ktx:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
 
-    implementation("androidx.camera:camera-core:1.3.0-alpha03")
-    implementation("androidx.camera:camera-camera2:1.3.0-alpha03")
-    implementation("androidx.camera:camera-lifecycle:1.3.0-alpha03")
-    implementation("androidx.camera:camera-extensions:1.3.0-alpha03")
-    implementation("androidx.camera:camera-view:1.3.0-alpha03")
+    implementation("androidx.camera:camera-core:1.2.2")
+    implementation("androidx.camera:camera-camera2:1.2.2")
+    implementation("androidx.camera:camera-lifecycle:1.2.2")
+    implementation("androidx.camera:camera-extensions:1.2.2")
+    implementation("androidx.camera:camera-view:1.2.2")
 
-    implementation("androidx.fragment:fragment:1.5.5")
-    implementation("androidx.fragment:fragment-ktx:1.5.5")
+    implementation("androidx.fragment:fragment:1.5.6")
+    implementation("androidx.fragment:fragment-ktx:1.5.6")
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
 
     implementation("io.coil-kt:coil:2.2.2"){
