0901a325b127ced0e5ef7f063d1682938f2d1cb8
n8fr8 <nathan@freitas.net>
2023-05-08 14:53:31 -0400

    update dependencies and lib package ids to org.proofmode (from org.witness)

--- android-libproofcam/build.gradle.kts
+++ android-libproofcam/build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 
-    implementation("com.google.android.material:material:1.9.0-beta01")
+    implementation("com.google.android.material:material:1.9.0-alpha02")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.core:core-ktx:1.10.0")
@@ -47,8 +47,8 @@ dependencies {
     implementation("androidx.camera:camera-extensions:1.2.2")
     implementation("androidx.camera:camera-view:1.2.2")
 
-    implementation("androidx.fragment:fragment:1.5.6")
-    implementation("androidx.fragment:fragment-ktx:1.5.6")
+    implementation("androidx.fragment:fragment:1.5.7")
+    implementation("androidx.fragment:fragment-ktx:1.5.7")
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
