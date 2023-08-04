ce4105a21603fb74f0ee22fed2d4cbd1bb7b3c9d
n8fr8 <nathan@guardianproject.info>
2023-08-04 12:28:55 -0400

    remove unused reference

--- android-libproofcam/src/main/java/org/witness/proofmode/camera/CameraActivity.kt
+++ android-libproofcam/src/main/java/org/witness/proofmode/camera/CameraActivity.kt
@@ -3,7 +3,6 @@ package org.witness.proofmode.camera
 import android.os.Bundle
 import android.view.KeyEvent
 import androidx.appcompat.app.AppCompatActivity
-import org.witness.proofmode.camera.fragments.OnVolumeKeyListener
 
 class CameraActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
