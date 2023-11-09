9075c4efcd66defef4df5ba2371a10573a1f2efc
n8fr8 <nathan@guardianproject.info>
2023-11-09 11:44:52 -0500

    handle null video thumbnail and IOException

--- app/src/main/java/Utils.kt
+++ app/src/main/java/Utils.kt
@@ -3,10 +3,12 @@ package org.witness.proofmode
 import android.content.ContentUris
 import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Build
 import android.provider.MediaStore
 import android.util.Size
+
 
 /**
  * Get a thumbnail for a video.
@@ -16,13 +18,25 @@ import android.util.Size
  * @return A Bitmap thumbnail.
  */
 fun getVideoThumbnail(context: Context, videoUri: Uri): Bitmap {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-        return context.contentResolver.loadThumbnail(videoUri, Size(840, 640), null)
-    }
-    return MediaStore.Video.Thumbnails.getThumbnail(
+
+    try {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            return context.contentResolver.loadThumbnail(videoUri, Size(840, 640), null)
+        }
+        return MediaStore.Video.Thumbnails.getThumbnail(
             context.contentResolver,
             ContentUris.parseId(videoUri),
             MediaStore.Video.Thumbnails.FULL_SCREEN_KIND,
             null
-    )
+        )
+    } catch (e: Exception) {
+
+        //couldn't load thumbnail for some reason
+        return BitmapFactory.decodeResource(
+            context.resources,
+            R.drawable.proofmodegrey
+        );
+    }
+
+
 }
