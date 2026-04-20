9e39488a8b5e467c574b214ba09838af8749a6ba
n8fr8 <nathan@freitas.net>
2026-04-20 13:23:26 -0400

    clean-up c2pa preference handling

--- app/src/main/java/org/witness/proofmode/SigningSettingsActivity.kt
+++ app/src/main/java/org/witness/proofmode/SigningSettingsActivity.kt
@@ -4,9 +4,11 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.CheckBoxPreference
 import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
+import androidx.preference.SwitchPreference
 import androidx.preference.SwitchPreferenceCompat
 import org.witness.proofmode.databinding.ActivitySettingsBinding
 import org.witness.proofmode.databinding.ActivitySigningSettingsBinding
@@ -75,10 +77,14 @@ class SigningSettingsActivity : AppCompatActivity() {
 
         private fun applyModeState() {
             val prefs = PreferenceManager.getDefaultSharedPreferences(requireContext())
+
+            /**
             val isRemote = prefs.getBoolean(
                 ProofMode.PREF_OPTION_REMOTE_SIGNING,
                 ProofMode.PREF_OPTION_REMOTE_SIGNING_DEFAULT
-            )
+            )**/
+
+            val isRemote = findPreference<SwitchPreferenceCompat>(ProofMode.PREF_OPTION_REMOTE_SIGNING)?.isChecked == true
 
             findPreference<EditTextPreference>(ProofMode.PREF_OPTION_PROOFSIGN_SERVER)
                 ?.isEnabled = isRemote
@@ -86,10 +92,6 @@ class SigningSettingsActivity : AppCompatActivity() {
             val tsaPref = findPreference<EditTextPreference>(ProofMode.PREF_OPTION_TSA_SERVER)
             tsaPref?.isEnabled = !isRemote
 
-            if (isRemote) {
-                // Remote signing always uses the pinned TSA.
-                tsaPref?.text = ProofMode.PREF_OPTION_TSA_SERVER_DEFAULT
-            }
         }
     }
 }
