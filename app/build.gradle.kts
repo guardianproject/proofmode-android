f630f044fb277dddc3bd68f3bb07e99da198630d
n8fr8 <nathan@freitas.net>
2026-05-22 15:41:46 -0400

    update to 3.0.4-BETA-5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "org.witness.proofmode"
         minSdk = 28
         targetSdk = 36
-        versionCode = 30420400
-        versionName = "3.0.4-BETA-4"
+        versionCode = 30420500
+        versionName = "3.0.4-BETA-5"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
