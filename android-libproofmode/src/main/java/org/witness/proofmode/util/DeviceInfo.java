cfce600da523bcb94f8adb98cd33e2c2fecc0b4b
n8fr8 <nathan@freitas.net>
2026-08-28 17:01:29 -0400

    fix for cellinfo NPE error #133

--- android-libproofmode/src/main/java/org/witness/proofmode/util/DeviceInfo.java
+++ android-libproofmode/src/main/java/org/witness/proofmode/util/DeviceInfo.java
@@ -589,28 +589,30 @@ public class DeviceInfo {
             List<CellInfo> infos = null;
             infos = tel.getAllCellInfo();
 
-            for (int i = 0; i < infos.size(); ++i) {
-                try {
-                    JSONObject cellObj = new JSONObject();
-                    CellInfo info = infos.get(i);
-                    if (info instanceof CellInfoGsm) {
-                        CellSignalStrengthGsm gsm = ((CellInfoGsm) info).getCellSignalStrength();
-                        CellIdentityGsm identityGsm = ((CellInfoGsm) info).getCellIdentity();
-                        cellObj.put("cellId", identityGsm.getCid());
-                        cellObj.put("lac", identityGsm.getLac());
-                        cellObj.put("dbm", gsm.getDbm());
-                        cellList.put(cellObj);
-                    } else if (info instanceof CellInfoLte) {
-                        CellSignalStrengthLte lte = ((CellInfoLte) info).getCellSignalStrength();
-                        CellIdentityLte identityLte = ((CellInfoLte) info).getCellIdentity();
-                        cellObj.put("cellId", identityLte.getCi());
-                        cellObj.put("tac", identityLte.getTac());
-                        cellObj.put("dbm", lte.getDbm());
-                        cellList.put(cellObj);
+            if (infos != null) {
+                for (int i = 0; i < infos.size(); ++i) {
+                    try {
+                        JSONObject cellObj = new JSONObject();
+                        CellInfo info = infos.get(i);
+                        if (info instanceof CellInfoGsm) {
+                            CellSignalStrengthGsm gsm = ((CellInfoGsm) info).getCellSignalStrength();
+                            CellIdentityGsm identityGsm = ((CellInfoGsm) info).getCellIdentity();
+                            cellObj.put("cellId", identityGsm.getCid());
+                            cellObj.put("lac", identityGsm.getLac());
+                            cellObj.put("dbm", gsm.getDbm());
+                            cellList.put(cellObj);
+                        } else if (info instanceof CellInfoLte) {
+                            CellSignalStrengthLte lte = ((CellInfoLte) info).getCellSignalStrength();
+                            CellIdentityLte identityLte = ((CellInfoLte) info).getCellIdentity();
+                            cellObj.put("cellId", identityLte.getCi());
+                            cellObj.put("tac", identityLte.getTac());
+                            cellObj.put("dbm", lte.getDbm());
+                            cellList.put(cellObj);
+                        }
+
+                    } catch (Exception ex) {
+
                     }
-
-                } catch (Exception ex) {
-
                 }
             }
         }
